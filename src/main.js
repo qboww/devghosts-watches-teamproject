86bdd8d02bbeaa6a16a2e98f1a17d91bbc9ee381
Artem Parashchuk <a.p.paraschuk@gmail.com>
2024-03-03 11:33:07 +0200

    Add js to mobile menu

--- src/main.js
+++ src/main.js
@@ -28,3 +28,48 @@ const arrowUp = {
 };
 
 arrowUp.listenScroll();
+
+const btnClose = document.querySelector('#close');
+const btnOpen = document.querySelector('.menu-link');
+const mobileMenuBg = document.querySelector('.mob-menu-background');
+const mobileMenu = document.querySelector('.mobile-menu');
+
+
+btnOpen.addEventListener('click', (e) => {
+ e.preventDefault();
+ mobileMenuBg.classList.add('is-open')
+ mobileMenu.classList.add('is-open')
+})
+
+btnClose.addEventListener('click', (e) => {
+  e.preventDefault();
+  mobileMenuBg.classList.remove('is-open')
+  mobileMenu.classList.remove('is-open')
+})
+
+
+
+
+
+
+
+// const mobileMenu = {
+//   btnClose: document.querySelector('#close'),
+
+//   btnOpen: document.querySelector('.menu-link'),
+
+//   mobileMenuBg: document.querySelector('.mob-menu-background'),
+
+//   mobileMenu: document.querySelector('.mobile-menu'),
+
+//   show: this.mobileMenuBg.add('is-open') && this.mobileMenu.add('is-open'),
+   
+//   close: this.mobileMenuBg.remove('is-open') && this.mobileMenu.remove('is-open'),
+
+//   listenMenu() {
+//     window.addEventListener('click', (e) => {
+//       e.preventDefault();
+
+//     })
+//   }
+// }
