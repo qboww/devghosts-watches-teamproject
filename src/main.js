a08018be01d63ae0c57ed80dd5f998f2a255948b
Yevhenii Sarancha <yevhenii.sarancha@outlook.com>
2024-04-30 19:39:33 +0300

    added aos anims

--- src/main.js
+++ src/main.js
@@ -2,8 +2,8 @@ import initArrowFeature from './js/arrow-logics';
 import initModalLogics from './js/modal-logics';
 import initShowMore from './js/home-to-catalog';
 
+AOS.init();
+
 initShowMore();
 initArrowFeature();
 initModalLogics();
-
-AOS.init();
