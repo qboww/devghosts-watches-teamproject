72574ae93ae4a9b33f241e849677c55e9504ee2a
Yevhenii Sarancha <yevhenii.sarancha@outlook.com>
2024-04-30 19:25:05 +0300

    fixed dot behavior

--- src/js/modal-logics.js
+++ src/js/modal-logics.js
@@ -3,11 +3,16 @@ function initModalLogics() {
   const btnOpen = document.querySelector('.menu-link');
   const mobileMenuBg = document.querySelector('.mob-menu-background');
   const mobileMenu = document.querySelector('.mobile-menu');
+  const homeLink = document.querySelector('.mob-menu-link[href="/index.html"]');
+  const catalogLink = document.querySelector('.mob-menu-link[href="./catalog-interactive.html"]');
 
   btnOpen.addEventListener('click', e => {
     e.preventDefault();
     mobileMenuBg.classList.add('is-open');
     mobileMenu.classList.add('is-open');
+
+    homeLink.classList.remove('current');
+    catalogLink.classList.add('current');
   });
 
   btnClose.addEventListener('click', e => {
