7b8bc8977d5ff382606036bea0af4bcfbd38128b
qboww <yevhenii.sarancha@outlook.com>
2024-05-01 12:19:43 +0000

    Deploying to gh-pages from @ qboww/devghosts-watches-teamproject@7ff974fd7244fb815ea72e41ba88c31699894579 🚀

---
+++ commonHelpers2.js
@@ -0,0 +1,2 @@
+import"./assets/main-f2f74635.js";const t=document.getElementById("show-more");t.addEventListener("click",()=>{window.location.href="./catalog-interactive.html"});
+//# sourceMappingURL=commonHelpers2.js.map
